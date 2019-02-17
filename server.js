02fccefc9d965cd96564c9ecba0df35c8c7786df
EdsonCandido <edson.c.a.j@hotmail.com>
2019-02-16 22:59:52 -0200

    Implemantada a função de Salvar no banco

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ console.log("TUDO SAFE POR AQUI");
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoCliente = require('mongodb').MongoClient;
+
+const uri = "mongodb://localhost:27017/crud-nodejs";
+
+MongoCliente.connect(uri, (err, client) =>{
+  //O serve roda aqui
+  if(err){
+    return console.log(err);
+  }
+  db = client.db('crud-nodejs');
+
+  app.listen(3000, ()=>{
+    console.log('Serve 3000');
+  });
+});
 
 app.use(bodyParser.urlencoded({extended : true}));
-
-app.listen(3000, function () {
-  console.log("PORT: 3000");
-});
 
 app.set('view engine', 'ejs');
 
@@ -15,6 +26,11 @@ app.get('/', (req, res) =>{
   res.render('index.ejs');
 });
 
+//SALVANDO DADOS NO BANCO DE DADOS 
 app.post('/show', (req, res) =>{
-  console.log(req.body);
+  db.collection('data').save(req.body , (err, result) =>{
+    if(err) return console.log(err);
+    console.log("Savo no banco ");
+    res.redirect('/');
+  });
 });
